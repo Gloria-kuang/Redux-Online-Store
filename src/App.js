805a2d67e1bb3b3d4eb7d3a43e7e7b740faec6d9
Chumei <gloria.kcm@gmail.com>
2022-08-24 11:29:17 -0400

    add product detail

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
